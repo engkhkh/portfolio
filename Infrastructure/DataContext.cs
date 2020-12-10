f5439839131b68e16343db0e9fbc5312bf4f72b9
engkhkh <khilel208@gmail.com>
2020-12-10 16:05:39 +0300

    10-12

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+   public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValue("NEWID()");
+            modelBuilder.Entity<portfolioitem>().Property(x => x.Id).HasDefaultValue("NEWID()");
+            modelBuilder.Entity<Owner>().HasData(
+                
+                new Owner()
+                {
+                    Id=Guid.NewGuid(),Avatar="AVATR1.JPEG",FullName="ITECH-CONSULTANTS",Profil="IT/CLOULD/MOBILE/ERP Systems"
+                }
+                
+                
+                );
+
+        }
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<portfolioitem> portfolioitems { get; set; }
+    }
+}
